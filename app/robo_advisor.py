eb261ffb241514a602109a0c60fb59a045e42042
wae10 <59625812+wae10@users.noreply.github.com>
2020-02-23 00:28:18 -0500

    Format latest day thru recent low output

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,3 @@
-
-
 # app/robo_advisor.py
 import requests
 import json
@@ -8,42 +6,70 @@ from dotenv import load_dotenv
 
 load_dotenv() # load env
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
 
-print("REQUESTING SOME DATA FROM THE INTERNET...")
+symbol = input("Welcome to the robo-advisor\nPlease enter your desired stock symbol: \n")
 
-symbol = "TSLA"
+symbol = symbol.upper()
 
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
-print("URL:", request_url)
+if len(symbol) != 4 or symbol.isnumeric():
+    print("THIS IS WRONG YOU FUCKER")
 
-response = requests.get(request_url)
-print(type(response))
-print(response.status_code)
-print(type(response.text)) #> str
+else:
 
-parsed_response = json.loads(response.text)
-print(type(parsed_response)) #> dict
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+    print("URL:", request_url)
 
-print(parsed_response)
-#breakpoint()
+    response = requests.get(request_url)
+    print(type(response))
+    print(response.status_code)
+    print(type(response.text)) #> str
 
-print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
-print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+    parsed_response = json.loads(response.text)
+
+    last_refresed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+    tsd = parsed_response["Time Series (Daily)"]
+
+    dates = list(tsd.keys())
+
+    latest_day = dates[0]
+
+    latest_close = tsd[latest_day]["4. close"]
+
+    high_prices = []
+    low_prices = [] 
+
+    for date in dates:
+        high_price = tsd[date]["2. high"]
+        low_price = tsd[date]["3. low"]
+        high_prices.append(float(high_price))
+        low_prices.append(float(low_price))
+
+    recent_high = max(high_prices)
+    recent_low = min(low_prices)
+
+    print("DICT:", parsed_response)
+
+    print("-------------------------")
+    print("SELECTED SYMBOL:",symbol)
+    print("-------------------------")
+    print("REQUESTING STOCK MARKET DATA...")
+    print("REQUEST AT: 2018-02-20 02:00pm")
+    print("-------------------------")
+    print(f"LATEST DAY: {last_refresed}")
+    print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+    print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+    print(f"RECENT LOW: {to_usd(float(recent_low))}")
+    print("-------------------------")
+    print("RECOMMENDATION: BUY!")
+    print("RECOMMENDATION REASON: TODO")
+    print("-------------------------")
+    print("HAPPY INVESTING!")
+    print("-------------------------")
 
 
 
