4d43a5624ea276589938509cab9e9db1894f66fd
wae10 <59625812+wae10@users.noreply.github.com>
2020-02-24 13:30:37 -0500

    Multiple stocks, multiple csv files

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -14,100 +14,124 @@ load_dotenv() # load env
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price)
 
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
-
-symbol = input("Welcome to the robo-advisor!\nPlease enter your desired stock symbol: \n")
-
 # function for symbol inputs like this: 'ad42'
 def num_there(s):
     return any(i.isdigit() for i in s)
 
-if len(symbol) > 5 or num_there(symbol):
-    print("Invalid symbol. Please try again.")
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
+
+# empty list for multiple stocks
+stocks = []
+
+stock_num = input("Welcome to the robo-advisor!\nHow many stocks would you like to test?")
+
+if num_there(stock_num):
+
+    if stock_num.isnumeric: #still need to fix for input like ad34
+        stock_num = eval(stock_num)
+        for i in range(1, stock_num + 1):
+            symbol = input("Please enter your desired stock symbol #" + str(i) + ":\n")
+            if len(symbol) > 5 or num_there(symbol):
+                print("Invalid symbol. Input will be discarded.")
+            else:
+                stocks.append(symbol)
+        print(stocks)
+    else:
+        print("Invalid symbol. Restarting program...")
+
+else:
+    print("Invalid symbol. Restarting program...")
+
+
+for symbol in stocks:
+    print(symbol)
 
 else:
 
-    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+    for symbol in stocks:
 
-    response = requests.get(request_url)
+        request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
 
-    parsed_response = json.loads(response.text)
+        response = requests.get(request_url)
 
-    last_refresed = parsed_response["Meta Data"]["3. Last Refreshed"]
+        parsed_response = json.loads(response.text)
 
-    tsd = parsed_response["Time Series (Daily)"]
+        last_refresed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-    dates = list(tsd.keys())
+        tsd = parsed_response["Time Series (Daily)"]
 
-    latest_day = dates[0]
+        dates = list(tsd.keys())
 
-    latest_close = tsd[latest_day]["4. close"]
+        latest_day = dates[0]
 
-    high_prices = []
-    low_prices = [] 
+        latest_close = tsd[latest_day]["4. close"]
 
-    for date in dates:
-        high_price = tsd[date]["2. high"]
-        low_price = tsd[date]["3. low"]
-        high_prices.append(float(high_price))
-        low_prices.append(float(low_price))
+        high_prices = []
+        low_prices = [] 
 
-    recent_high = max(high_prices)
-    recent_low = min(low_prices)
+        for date in dates:
+            high_price = tsd[date]["2. high"]
+            low_price = tsd[date]["3. low"]
+            high_prices.append(float(high_price))
+            low_prices.append(float(low_price))
 
-    # for comparing latest close to recent 80% of recent high
-    HIGHthreshold = recent_high * (8/10)
+        recent_high = max(high_prices)
+        recent_low = min(low_prices)
 
-    # for comparing low
-    LOWthreshold = recent_low * 1.2
+        # for comparing latest close to recent 80% of recent high
+        HIGHthreshold = recent_high * (8/10)
 
-    if eval(latest_close) < HIGHthreshold and eval(latest_close) > LOWthreshold:
-        recommendation = "HOLD"
-        reason = "STOCK IS LESS THAN 80% OF THE RECENT HIGH AND MORE THAN 120% OF THE RECENT LOW"
-    elif eval(latest_close) >= HIGHthreshold:
-        recommendation = "SELL!"
-        reason = "STOCK IS OVERVALUED BECAUSE IT IS GREATER THAN 80% OF THE RECENT HIGH" 
-    elif eval(latest_close) <= LOWthreshold:
-        recommendation = "BUY!"
-        reason = "STOCK IS UNDERVALUED BECAUSE IT IS WITHIN 120% OF THE RECENT LOW"
+        # for comparing low
+        LOWthreshold = recent_low * 1.2
 
-    print("-------------------------")
-    print("SELECTED SYMBOL:",symbol)
-    print("-------------------------")
-    print("REQUESTING STOCK MARKET DATA...")
-    print("REQUEST AT:", now)
-    print("-------------------------")
-    print(f"LATEST DAY: {last_refresed}")
-    print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-    print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-    print(f"RECENT LOW: {to_usd(float(recent_low))}")
-    print("-------------------------")
-    print("RECOMMENDATION: " + recommendation)
-    print("RECOMMENDATION REASON: " + reason)
-    print("-------------------------")
-    print("HAPPY INVESTING!")
-    print("-------------------------")
+        if eval(latest_close) < HIGHthreshold and eval(latest_close) > LOWthreshold:
+            recommendation = "HOLD"
+            reason = "STOCK IS LESS THAN 80% OF THE RECENT HIGH AND MORE THAN 120% OF THE RECENT LOW"
+        elif eval(latest_close) >= HIGHthreshold:
+            recommendation = "SELL!"
+            reason = "STOCK IS OVERVALUED BECAUSE IT IS GREATER THAN 80% OF THE RECENT HIGH" 
+        elif eval(latest_close) <= LOWthreshold:
+            recommendation = "BUY!"
+            reason = "STOCK IS UNDERVALUED BECAUSE IT IS WITHIN 120% OF THE RECENT LOW"
 
-    # csv stuff
-    filename = open("prices.csv", 'w+')
+        print("-------------------------")
+        print("SELECTED SYMBOL:",symbol)
+        print("-------------------------")
+        print("REQUESTING STOCK MARKET DATA...")
+        print("REQUEST AT:", now)
+        print("-------------------------")
+        print(f"LATEST DAY: {last_refresed}")
+        print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+        print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+        print(f"RECENT LOW: {to_usd(float(recent_low))}")
+        print("-------------------------")
+        print("RECOMMENDATION: " + recommendation)
+        print("RECOMMENDATION REASON: " + reason)
+        print("-------------------------")
+        print("HAPPY INVESTING!")
+        print("-------------------------")
 
-    filename.write('timestamp, open, high, low, close, volume\n')
+        # csv stuff
+        file = "prices_" + symbol + ".csv"
+        filename = open(file, 'w+')
 
-    for date in dates:
-        filename.write(date)
-        filename.write(", ")
-        filename.write(tsd[date]["1. open"])
-        filename.write(", ")
-        filename.write(tsd[date]["2. high"])
-        filename.write(", ")
-        filename.write(tsd[date]["3. low"])
-        filename.write(", ")
-        filename.write(tsd[date]["4. close"])
-        filename.write(", ")
-        filename.write(tsd[date]["5. volume"])
-        filename.write("\n")
+        filename.write('timestamp, open, high, low, close, volume\n')
 
-    
+        for date in dates:
+            filename.write(date)
+            filename.write(", ")
+            filename.write(tsd[date]["1. open"])
+            filename.write(", ")
+            filename.write(tsd[date]["2. high"])
+            filename.write(", ")
+            filename.write(tsd[date]["3. low"])
+            filename.write(", ")
+            filename.write(tsd[date]["4. close"])
+            filename.write(", ")
+            filename.write(tsd[date]["5. volume"])
+            filename.write("\n")
+
+        
 
 
 
