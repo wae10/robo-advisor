b27d47c495feae4e7d0e0c589a7bd7efb1a657d2
wae10 <59625812+wae10@users.noreply.github.com>
2020-04-15 21:45:26 -0500

    Travis fix

--- test/my_test.py
+++ test/my_test.py
@@ -1,7 +1,7 @@
 # robo-advisor/my_test.py
 
 from app.robo_advisor import to_usd, num_there, get_response, transform_response, write_to_csv
-import os, csv
+import os, requests
 
 def test_to_usd():
     result = to_usd(3.50)
@@ -13,17 +13,27 @@ def test_num_there():
 
 #test that get_response() returns a dictionary containing the following
 def test_get_response():
-    result = get_response("tsla", "BCV3KUW4GBOD7DTL")
+    symbol = "tsla"
+    API_KEY = "BCV3KUW4GBOD7DTL"
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+    result = get_response(symbol, API_KEY, request_url)
     assert type(result) is dict and 'Meta Data' in result and 'Time Series (Daily)' in result
 
 #test that transform_response returns a list and contains 'open' in the first element
 def test_transform_response():
-    parsed_response = get_response("tsla", "BCV3KUW4GBOD7DTL")
+    symbol = "tsla"
+    API_KEY = "BCV3KUW4GBOD7DTL"
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+    parsed_response = get_response(symbol, API_KEY, request_url)
     result = transform_response(parsed_response)
     assert type(result) is list and 'open' in result[0]
 
 def test_write_to_csv():
-    rows = transform_response(get_response("tsla", "BCV3KUW4GBOD7DTL"))
+    symbol = "tsla"
+    API_KEY = "BCV3KUW4GBOD7DTL"
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+
+    rows = transform_response(get_response(symbol, API_KEY, request_url))
 
     csv_file_path = os.path.join("/Users/williameverett/Desktop/Georgetown-University/Spring-2020/OPIM-243/robo-advisor/app/data/prices.csv")
 
